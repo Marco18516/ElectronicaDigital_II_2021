c8def653f9e9c15c9dcc68c40a78002fce7aeb30
Marco18516 <78225706+Marco18516@users.noreply.github.com>
2021-02-04 14:13:10 -0600

    Lab 1 Terminado

---
+++ Lab02/Lab02.X/Laboratorio_02.c
@@ -0,0 +1,69 @@
+/* LABORATORIO #2
+ * File:   Lab01.c
+ * Author: Marco Juárez
+ * Carné: 18516
+ */
+
+//Librerías
+//******************************************************************************
+#include <xc.h>
+
+//******************************************************************************
+// Configuración
+//******************************************************************************
+// CONFIG1
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator: Crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+//******************************************************************************
+// Variables
+//******************************************************************************
+#define _XTAL_FREQ 8000000
+int i;                          // Definimos variable "i" como entero
+int run (unsigned int Cont); //Variable para conteo
+
+//******************************************************************************
+// Prototipos de funciones
+//******************************************************************************
+void init_int (){
+    INTCONbits.RBIE = 1; // Habilitamos interrupt on change
+    INTCONbits.GIE = 1; // Habilitamos la interrupcion global
+    OPTION_REGbits.nRBPU = 0; // turn on pull-up
+     
+}
+
+void __interrupt() isr(void) {
+    if (INTCONbits.RBIF) 
+    {
+        for(i=0; i<=255; ++i){  // Declaramos FOR incrementando de 0 a 255
+
+            PORTB = i;          // Declaramos la salida del PIN_B0 en alto
+
+            __delay_ms(50);    // Tiempo de espera de 500ms
+
+        }
+        INTCONbits.RBIF = 0;
+    }
+}
+
+
+
+void main(void) {
+    TRISB = 0x00;
+    init_int();
+    while (1) 
+    {
+        
+    }
+    return;
+}
